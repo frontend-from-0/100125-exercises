da92026405f4c5260bb7f51d3ed51b7f8b4bf322
batibatii <nbatikansevil@gmail.com>
2025-05-23 11:30:40 +0300

    Refactor showAllFlashcards and practiceFlashcard functions for improved output formatting and error handling

--- Lesson17/batikan/flashcards.js
+++ Lesson17/batikan/flashcards.js
@@ -47,24 +47,24 @@ function showAllFlashcards() {
   let maxQuestionLength = 0;
   let maxAnswerLength = 0;
   for (const flashcard of flashcards) {
-    if (flashcard.question.length > maxQuestionLength){
+    if (flashcard.question.length > maxQuestionLength) {
       maxQuestionLength = flashcard.question.length;
     }
-    if (flashcard.answer.length > maxAnswerLength){
+    if (flashcard.answer.length > maxAnswerLength) {
       maxAnswerLength = flashcard.answer.length;
     }
   }
-    
-
-maxQuestionLength += 2;
-maxAnswerLength += 2;
 
 
-  for(const flashcard of flashcards){
-      const paddedQuestion = flashcard.question.padEnd(maxQuestionLength, ' ');
-      const paddedAnswer = flashcard.answer.padEnd(maxAnswerLength, ' ');
-      const topicInfo = flashcard.topic ? `[Topic: \'${flashcard.topic}\']` : '[No topic]';
-      console.log(` ${paddedQuestion} --- ${paddedAnswer} --- ${topicInfo}`);
+  maxQuestionLength += 2;
+  maxAnswerLength += 2;
+
+
+  for (const flashcard of flashcards) {
+    const paddedQuestion = flashcard.question.padEnd(maxQuestionLength, ' ');
+    const paddedAnswer = flashcard.answer.padEnd(maxAnswerLength, ' ');
+    const topicInfo = flashcard.topic ? `[Topic: \'${flashcard.topic}\']` : '[No topic]';
+    console.log(` ${paddedQuestion} --- ${paddedAnswer} --- ${topicInfo}`);
   }
   console.log();
 }
@@ -82,7 +82,6 @@ Function: addFlashcard(question, answer)
 function addFlashcard(question, answer) {
   console.log();
   console.log('Adding a new flashcards...');
-
   const flashcard = {
     question: question,
     answer: answer,
@@ -116,27 +115,30 @@ Function: practiceFlashcard(index)
 NOTE: For simplicity, we'll just log both.
 */
 
-/*Now question and all the answers appearing at the same time.*/
+
 
 function practiceFlashcard(index) {
   if (index >= 0 && index < flashcards.length) {
+    console.log('-------');
+    console.log();
+    console.log('Question: ', flashcards[index].question);
+    console.log();
     setTimeout(() => {
-      console.log('-------');
       console.log();
-      console.log('Question: ', flashcards[index].question);
+      console.log('------')
       console.log();
+      console.log('*** This section belongs to STEP 4 ***')
       console.log('The answer is:', flashcards[index].answer);
     }, 3000);
-    } else {
-    setTimeout( () => {
+  } else {
     console.log(`*** Invalid Index: Flashcard with index \'${index}\' is not in the list. Use index from 0 to ${flashcards.length - 1} ***`);
     console.log();
     console.log('-------');
-    },3000);
+
   }
 }
 
-practiceFlashcard(2);
+practiceFlashcard(1);
 practiceFlashcard(5);
 /*
 -----------------------------------------------------------
@@ -147,34 +149,24 @@ Function: updateFlashcard(question, newQuestion, newAnswer)
 - Logs if updated or not found.
 */
 
-function updateFlashcard(question, newQuestion, newAnswer){
-  for(let i = 0; i < flashcards.length; i++){
-    if (flashcards[i].question === question){
-      setTimeout(() => {
+function updateFlashcard(question, newQuestion, newAnswer) {
+  for (let i = 0; i < flashcards.length; i++) {
+    if (flashcards[i].question === question) {
       console.log();
-      console.log(` Question: \'${question}\' is found. Wait a second..`);
+      console.log(`Question: \'${question}\' is found. Wait a second..`);
       flashcards[i].question = newQuestion;
       flashcards[i].answer = newAnswer;
-      }, 3500);
-      setTimeout(() => {
-        console.log(`New question '${newQuestion}' and new answer '${newAnswer}' have been set.`);
-        console.log();
-      }, 4000);
-      
-      setTimeout(() => {
-        showAllFlashcards();
-        console.log('------')
-      }, 4500);
-      
+      console.log(`New question '${newQuestion}' and new answer '${newAnswer}' have been set.`);
+      console.log();
+      showAllFlashcards();
+      console.log('------')
       return;
     }
   }
-  setTimeout(() => {      
   console.log();
-  console.log(`'${question}' is not found.`);    
-}, 3500);
+  console.log(`'${question}' is not found.`);
 }
- 
+
 updateFlashcard('What is HTML?', 'What is C++?', 'C++ is a high-level, general-purpose programming language');
 updateFlashcard('What is C#', 'What is Java?', 'Java is a high-level, general-purpose, memory-safe, object-oriented programming language.');
 
@@ -188,25 +180,24 @@ Function: removeFlashcard(question)
 - Logs if removed or not found.
 */
 
-function removeFlashcard(question){
+function removeFlashcard(question) {
   console.log();
-  setTimeout(() => {
-  for(let i = 0; i < flashcards.length; i++){
-    if (flashcards[i].question === question){
+  console.log('------');
+  for (let i = 0; i < flashcards.length; i++) {
+    if (flashcards[i].question === question) {
       let foundQuestion = flashcards.indexOf(flashcards[i])
       flashcards.splice(foundQuestion, 1)
       console.log();
       console.log(`Question: \'${question}\' has been removed`)
       console.log();
       return;
-      } 
     }
-      console.log(`Question: \'${question}\' is not found.`)
-      showAllFlashcards();
-      console.log('------')
-    }, 5000)
   }
- 
+  console.log(`Question: \'${question}\' is not found.`)
+  showAllFlashcards();
+  console.log('------')
+}
+
 
 removeFlashcard('What does CSS do?');
 removeFlashcard('What is compiled mean?');
@@ -215,35 +206,29 @@ removeFlashcard('What is compiled mean?');
 /*CHOOSE A RANDOM FLASHCARD EACH TIME */
 
 function getRandomFlashcard() {
-  setTimeout(() => {
   console.log();
   console.log('Selecting a random flashcard...');
-  
   const randomIndex = Math.floor(Math.random() * flashcards.length);
   const randomCard = flashcards[randomIndex];
-  
   console.log(`Selected card #${randomIndex}: \"${randomCard.question}\"`);
-  }, 6000);
 }
 
 getRandomFlashcard();
 
 /* ADDING TOP OR TOPIC TO FLASHCARD */
 
-function addTopic(cardIndex, topic){
-  setTimeout(() => {
+function addTopic(cardIndex, topic) {
+  console.log('------');
   console.log();
-      if (cardIndex < 0 || cardIndex >= flashcards.length || typeof cardIndex !== 'number'){
-      console.log(`\'${cardIndex}\' is not a number or index can't found. Please enter a number between '0' and \'${flashcards.length - 1}\'`);
-      return;
-    } 
-
+  if (cardIndex < 0 || cardIndex >= flashcards.length || typeof cardIndex !== 'number') {
+    console.log(`\'${cardIndex}\' is not a number or index can't found. Please enter a number between '0' and \'${flashcards.length - 1}\'`);
+    return;
+  }
   flashcards[cardIndex].topic = topic;
   console.log(`Topic name: \'${topic}\` is added.`);
   showAllFlashcards();
   console.log();
   console.log('------')
-  }, 6000);
 }
 
 addTopic(4, 'HTML')
@@ -252,24 +237,17 @@ addTopic(1, 'JS');
 
 /* FLIP MODE */
 
-function flipCards(cardIndex){  
-      console.log();
-      if (cardIndex < 0 || cardIndex >= flashcards.length || typeof cardIndex !== 'number'){
-        setTimeout(() => {
-      console.log();
-      console.log(`\'${cardIndex}\' is not a number or index can't found. Please enter a number between '0' and \'${flashcards.length - 1}\'`);
-  }, 6500);
-      return  } else {
-        setTimeout(() => {
-      if (cardIndex >= 0 || cardIndex < flashcards.length){
-      console.log();
+function flipCards(cardIndex) {
+  console.log();
+  if (cardIndex < 0 || cardIndex >= flashcards.length || typeof cardIndex !== 'number') {
+    console.log(`\'${cardIndex}\' is not a number or index can't found. Please enter a number between '0' and \'${flashcards.length - 1}\'`);
+    return
+  } else {
+    if (cardIndex >= 0 || cardIndex < flashcards.length) {
       console.log(`Question: \'${flashcards[cardIndex].question}\'`);
     }
-  }, 7000);
-        setTimeout(() => {
-      console.log(`Answer: \'${flashcards[cardIndex].answer}\'`)
-  },8000);
-    } 
+    console.log(`Answer: \'${flashcards[cardIndex].answer}\'`)
+  }
 }
 
 

cf0ac28d1aa9f64f1467e4dafc441dc802ebbe8c
batikan <nbatikansevil@gmail.com>
2025-07-07 16:41:46 +0300

    Refactor Lesson 23 product cart implementation and clean up unused code

--- Lesson23/script.js
+++ Lesson23/script.js
@@ -1,55 +1,3 @@
-// Increment quantity of an element in the cart when "add to cart button" on a product is clicked
-
-// If remove item from cart is clicked, we will hide the item and set quantity to 0
-
-// If we click 'clear cart', then we hide all elements in the cart
-
-// If we add/remove an items to the cart, total price should be updated
-
-/* const products = [
-  {
-    name: 'apples',
-    addButtonId: 'apples_add',
-    quantityId: 'apples_quantity',
-    removeButtonId: 'apples_remove',
-    cartId: 'apples_cart',
-    price: 5,
-    decrease: 'apples_decrease',
-    increase: 'apples_increase',
-  },
-  {
-    name: 'bananas',
-    addButtonId: 'bananas_add',
-    quantityId: 'bananas_quantity',
-    removeButtonId: 'bananas_remove',
-    cartId: 'bananas_cart',
-    price: 3,
-    decrease: 'bananas_decrease',
-    increase: 'bananas_increase',
-  },
-  {
-    name: 'bread',
-    addButtonId: 'bread_add',
-    quantityId: 'bread_quantity',
-    removeButtonId: 'bread_remove',
-    cartId: 'bread_cart',
-    price: 4,
-    decrease: 'bread_decrease',
-    increase: 'bread_increase',
-  },
-  {
-    name: 'eggs',
-    addButtonId: 'eggs_add',
-    quantityId: 'eggs_quantity',
-    removeButtonId: 'eggs_remove',
-    cartId: 'eggs_cart',
-    price: 4,
-    decrease: 'eggs_decrease',
-    increase: 'eggs_increase',
-  },
-];
-*/
-
 const apiRequest = fetch('https://dummyjson.com/products?limit=10')
   .then((res) => res.json())
   .then((data) => {
@@ -63,26 +11,30 @@ const apiRequest = fetch('https://dummyjson.com/products?limit=10')
 
 console.log('apiRequest', apiRequest);
 
- /* function createProductCart(product) {
+function createProductCart(product) {
   const row = document.createElement('div');
-  row.classList.add(
-    'p-4',
-    'flex',
-    'items-stretch',
-    'justify-between',
-    'gap-4',
-    'rounded-xl',
-  );
+  row.classList.add('p-4');
 
   const div2 = document.createElement('div');
-  div2.classList.add('flex', 'flex-[2_2_0px]', 'flex-col', 'gap-4');
+  div2.classList.add('flex', 'items-stretch', 'justify-between', 'gap-4', 'rounded-xl');
+
+  const imgContainer = document.createElement('div');
+  imgContainer.classList.add('w-full', 'bg-center', 'bg-no-repeat', 'aspect-video', 'bg-cover', 'rounded-xl', 'flex-1');
+  imgContainer.style.backgroundImage = "url('https://lh3.googleusercontent.com/aida-public/AB6AXuD1QNTi5731zfjftg6462ZUTS-SNKWyZaBemhWwguzCJ2rBvyQFkqQDK9q_R8IYTBo7pWk-BZWSMK8X0wW53ca4LMyGtLHRxNKx-2fPDY6p9sKITYLuYf5lHmvNkBFyeeBKBIuZuU7hyjnGufzHZNLygZTG0a_KsGetu6bAm6_FF03MUYHfAhWWEwGNGcBHMD1s3TtOKOEcV8_d_scuDtgluGfYzgfnc0KEd3wojK4zRr3j4gQBfXTmD81v-7uqWq8Fk7DgmSA7ixs')";
 
   const div3 = document.createElement('div');
-  div3.classList.add('flex', 'flex-col', 'gap-1');
+  div3.classList.add('flex', 'flex-[2_2_0px]', 'flex-col', 'gap-4');
 
-  const h2 = document.createElement('h2');
-  h2.classList.add('text-[#1b0e0f]', 'text-base', 'font-bold', 'leading-tight');
-  h2.textContent = product.title;
+  const buttonParentContainer = document.createElement('div');
+  buttonParentContainer.classList.add('flex', 'gap-1', 'items-center');
+
+  const currencySign = document.createElement('span');
+  currencySign.classList.add('text-sm', 'mr-0');
+  currencySign.textContent = '$';
+
+  const price = document.createElement('span');
+  price.classList.add('mr-4');
+  price.textContent = product.price;
 
   const button = document.createElement('button');
   button.classList.add(
@@ -106,141 +58,160 @@ console.log('apiRequest', apiRequest);
   );
   button.textContent = 'Add to Cart';
 
-  div3.appendChild(h2);
-  div2.appendChild(div3);
-  div2.appendChild(button);
-  row.appendChild(div2);
 
-  document.getElementById('main').appendChild(row);
+  const div4 = document.createElement('div');
+  div4.classList.add('flex', 'flex-col', 'gap-4');
+
+  const h2 = document.createElement('h2');
+  h2.classList.add('text-[#1b0e0f]', 'text-base', 'font-bold', 'leading-tight');
+  h2.textContent = product.title;
+
+  const productDetails = document.createElement('p');
+  productDetails.classList.add('text-[#974e52]', 'text-sm', 'font-normal', 'leading-6');
+  productDetails.textContent = product.description;
+
+
+  div4.appendChild(h2)
+  div4.appendChild(productDetails)
+  div3.appendChild(div4);
+  buttonParentContainer.appendChild(currencySign);
+  buttonParentContainer.appendChild(price);
+  buttonParentContainer.appendChild(button);
+  div3.appendChild(buttonParentContainer);
+  div2.appendChild(div3);
+  div2.appendChild(imgContainer);
+  row.appendChild(div2);
+  document.querySelector('main').appendChild(row);
 }
 
-*/
 
-const totalPriceElement = document.getElementById('totalPriceId');
+
+/* const totalPriceElement = document.getElementById('totalPriceId');
 let total = parseInt(totalPriceElement.textContent);
 const clearAllButton = document.getElementById('clearAllButton');
 
 
 products.map((product) => {
-  const addButton = document.getElementById(product.addButtonId);
-  const removeButton = document.getElementById(product.removeButtonId);
-  const decreaseButton = document.getElementById(product.decrease);
-  const increaseButton = document.getElementById(product.increase);
+ const addButton = document.getElementById(product.addButtonId);
+ const removeButton = document.getElementById(product.removeButtonId);
+ const decreaseButton = document.getElementById(product.decrease);
+ const increaseButton = document.getElementById(product.increase);
 
 
-  if (addButton) {
-    addButton.addEventListener('click', () => {
-      addToCart(product.cartId, product.quantityId);
-      addTotalPrice(product.price);
-    })
-  }
+ if (addButton) {
+   addButton.addEventListener('click', () => {
+     addToCart(product.cartId, product.quantityId);
+     addTotalPrice(product.price);
+   })
+ }
 
-  if (removeButton) {
-    removeButton.addEventListener('click', () => {
-      removeFromCart(product.cartId, product.quantityId, product.price);
-    })
-  }
+ if (removeButton) {
+   removeButton.addEventListener('click', () => {
+     removeFromCart(product.cartId, product.quantityId, product.price);
+   })
+ }
 
-  if (decreaseButton) {
-    decreaseButton.addEventListener('click', () => {
-      decreaseQuantity(product.quantityId, product.cartId, product.price);
-    })
-  }
+ if (decreaseButton) {
+   decreaseButton.addEventListener('click', () => {
+     decreaseQuantity(product.quantityId, product.cartId, product.price);
+   })
+ }
 
-  if (increaseButton) {
-    increaseButton.addEventListener('click', () => {
-      increaseQuantity(product.quantityId, product.price);
-    })
-  }
-});
+ if (increaseButton) {
+   increaseButton.addEventListener('click', () => {
+     increaseQuantity(product.quantityId, product.price);
+   })
+ }
+}); 
 
 clearAllButton.addEventListener('click', () => clearAllCart());
 
 
 
 function addToCart(cartId, quantityId) {
-  const productCart = document.getElementById(cartId);
-  const quantityElement = document.getElementById(quantityId);
-  let quantity = parseInt(quantityElement.textContent);
+ const productCart = document.getElementById(cartId);
+ const quantityElement = document.getElementById(quantityId);
+ let quantity = parseInt(quantityElement.textContent);
 
-  quantity++;
+ quantity++;
 
-  productCart.classList.remove('hidden');
-  quantityElement.textContent = quantity;
+ productCart.classList.remove('hidden');
+ quantityElement.textContent = quantity;
 
 }
 
 function removeFromCart(cartId, quantityId, price) {
-  const productCart = document.getElementById(cartId);
-  const quantityElement = document.getElementById(quantityId);
-  let currentQuantity = parseInt(quantityElement.textContent);
+ const productCart = document.getElementById(cartId);
+ const quantityElement = document.getElementById(quantityId);
+ let currentQuantity = parseInt(quantityElement.textContent);
 
-  if (currentQuantity > 0) {
-    subtractTotalPrice(currentQuantity * price);
-    quantityElement.textContent = 0;
-    productCart.classList.add('hidden');
-  }
+ if (currentQuantity > 0) {
+   subtractTotalPrice(currentQuantity * price);
+   quantityElement.textContent = 0;
+   productCart.classList.add('hidden');
+ }
 
-  if (currentQuantity === 0) {
-    productCart.classList.add('hidden');
-  }
+ if (currentQuantity === 0) {
+   productCart.classList.add('hidden');
+ }
 }
 
-function decreaseQuantity(quantityId, cartId, price) {
+ function decreaseQuantity(quantityId, cartId, price) {
 
-  const productCart = document.getElementById(cartId)
-  const quantityElement = document.getElementById(quantityId);
-  let currentQuantity = parseInt(quantityElement.textContent);
+ const productCart = document.getElementById(cartId)
+ const quantityElement = document.getElementById(quantityId);
+ let currentQuantity = parseInt(quantityElement.textContent);
 
-  if (currentQuantity > 1) {
-    currentQuantity--;
-    quantityElement.textContent = currentQuantity;
-    subtractTotalPrice(price);
-  } else if (currentQuantity === 1) {
-    currentQuantity--;
-    quantityElement.textContent = currentQuantity;
-    subtractTotalPrice(price);
-    productCart.classList.add('hidden');
-  }
+ if (currentQuantity > 1) {
+   currentQuantity--;
+   quantityElement.textContent = currentQuantity;
+   subtractTotalPrice(price);
+ } else if (currentQuantity === 1) {
+   currentQuantity--;
+   quantityElement.textContent = currentQuantity;
+   subtractTotalPrice(price);
+   productCart.classList.add('hidden');
+ }
 }
 
 function increaseQuantity(quantityId, price) {
-  const quantityElement = document.getElementById(quantityId);
-  let quantity = parseInt(quantityElement.textContent);
+ const quantityElement = document.getElementById(quantityId);
+ let quantity = parseInt(quantityElement.textContent);
 
-  quantity++;
+ quantity++;
 
-  quantityElement.textContent = quantity;
-  addTotalPrice(price)
+ quantityElement.textContent = quantity;
+ addTotalPrice(price)
 }
 
 function addTotalPrice(price) {
 
-  total += price;
-  totalPriceElement.textContent = total;
+ total += price;
+ totalPriceElement.textContent = total;
 }
 
 function subtractTotalPrice(price) {
-  total -= price;
+ total -= price;
 
-  if (total < 0) total = 0;
-  totalPriceElement.textContent = total;
+ if (total < 0) total = 0;
+ totalPriceElement.textContent = total;
 }
 
 
 
 function clearAllCart() {
 
-  products.forEach(product => {
-    const quantityElement = document.getElementById(product.quantityId);
-    const cartElement = document.getElementById(product.cartId);
+ products.forEach(product => {
+   const quantityElement = document.getElementById(product.quantityId);
+   const cartElement = document.getElementById(product.cartId);
 
-    if (quantityElement) quantityElement.textContent = 0;
-    if (cartElement) cartElement.classList.add('hidden');
-  });
+   if (quantityElement) quantityElement.textContent = 0;
+   if (cartElement) cartElement.classList.add('hidden');
+ });
 
-  total = 0;
-  totalPriceElement.textContent = 0;
+ total = 0;
+ totalPriceElement.textContent = 0;
 
 }
 
+*/
